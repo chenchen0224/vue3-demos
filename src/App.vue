a879b02f458fbab15104d61b96e18065ba0f3c53
chenxinhua <1293485100@qq.com>
2023-01-09 19:49:05 +0800

    feat: init project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <!-- <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav> -->
+    <RouterView />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+</script>
+
+<style scoped>
+#app {
+  width: 100%;
+  height: 100%;
+  color: #333333;
+}
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Demos from "../views/demos/index.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      redirect: "/demos",
+    },
+    {
+      path: "/demos",
+      name: "demos",
+      component: Demos,
+    },
+  ],
+});
+
+export default router;
