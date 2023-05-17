29bf520f3b2494438494ec6eab57237ce5689563
chenxinhua <1293485100@qq.com>
2023-05-17 23:50:51 +0800

    feat: 配置eslint

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  printWidth: 150,
+  // 指定每个缩进级别的空格数
+  tabWidth: 2,
+  // 使用制表符而不是空格缩进行
+  useTabs: false,
+  // 在语句末尾打印分号
+  semi: false,
+  // 使用单引号而不是双引号
+  singleQuote: true,
+  // 在JSX中使用单引号而不是双引号
+  jsxSingleQuote: false,
+  // 多行时尽可能去掉尾随逗号。可选值"<none|es5|all>"，默认none
+  trailingComma: 'none',
+}
